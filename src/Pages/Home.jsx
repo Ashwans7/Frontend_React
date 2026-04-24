220ec3d01403eb61be95d4de0506c13208e7a88f
Ashwans7 <ashwanshrestha.kun@gmail.com>
2026-04-24 21:44:47 +0545

    added card navbar and useEffect for api

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,21 @@
+import { useEffect } from "react"
 import Card from "../Components/Card"
 import Navbar from "../Components/NavBar"
+import axios from "axios"
 
 
 function Home(){
+    const fetchBlogs= async()=>{
+       const response = await axios.get("http://localhost:3000/blog")
+
+    }
+    console.log(fetchBlogs)
+    
+    
+    useEffect(()=>{
+        fetchBlogs()
+
+    },[])
 return(
 <>
 <Navbar/>
