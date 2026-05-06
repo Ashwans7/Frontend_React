a57668e8cfc44feff3249bcd5093eadf76b4a912
Ashwans7 <ashwanshrestha.kun@gmail.com>
2026-05-06 17:31:13 +0545

    added a new url

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'https://devdiary-jmqa.onrender.com/';
 
 export const api = axios.create({
   baseURL: BASE_URL,
